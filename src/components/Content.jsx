44c533c03843e193ffaf86f1452d62a7f706e1a9
tiwavaldese <tiwaval7@gmail.com>
2024-09-05 10:58:35 +0100

    designing the content component

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -59,49 +59,56 @@ const Content = () => {
         </div>
         </div>
         </div>
+        <div className='flex flex-col md:flex-row px-3 lg:px-16  gap-3 lg:gap-1 lg:flex-row justify-between'>
+          <div className='flex flex-col'>
         <div className='flex justify-center '>
-        <img className=' w-[300px] h-[253px] ' src={bitmap}/>
+        <img className=' w-[300px] md:w-[350px] lg:w-[540px] h-[253px] lg:h-[400px] ' src={bitmap}/>
       </div>
-      <div>
-        <h2>DESIGN PORTFOLIO</h2>
-       <div>
+      <div className=' py-5 px-2 '>
+        <h2 className=' text-2xl md:text-2xl lg:text-4xl '>DESIGN PORTFOLIO</h2>
+       <div className='flex flex-row gap-10 py-2'>
         <p>HTML</p>
         <p>CSS</p>
         </div>
       </div>
-      <div>
-        <div>
+      <div className='flex flex-row gap-10 px-2 ' >
+        <div className='block lg:hidden'>
         <p>VIEW PROJECT</p>
-        <hr/>
+        <hr className='w-[100px]  md:w-[100px] my-2 border-[green] border-t-2 font-bold ' />
         </div>
-        <div>
+        <div className='block lg:hidden'>
         <p>VIEW CODE</p>
-        <hr/>
+        <hr className='w-[100px] md:w-[90px] my-2 border-[green] border-t-2 font-bold ' />
         </div>
       </div>
-      
-      <div  >
-        <img src={bitmap1} className='w-345 h-253 left-15' />
-        <h2>E-LEARNING LANDING PAGE</h2>
-        <div>
+      </div>
+      <div className='flex flex-col'>
+      <div className='flex justify-center'>
+        <img className=' w-[300px] md:w-[350px] lg:w-[540px] h-[253px] lg:h-[400px] ' src={bitmap1}  />
+        </div>
+        <div className=' py-5 px-2 '>
+        <h2 className=' text-1xl md:text-2xl lg:text-4xl '>E-LEARNING LANDING PAGE</h2>
+        <div className='flex flex-row gap-10 py-2'>
           <p>HTML</p>
           <p>CSS</p>
         </div>
-        <div>
-        <div>
+        </div>
+        <div className='flex flex-row px-2 gap-10'>
+        <div className='block lg:hidden'>
           <p>VIEW PROJECT</p>
-          <hr/>
+          <hr className='w-[100px] md:w-[110px] my-2 border-[green] border-t-2 font-bold ' />
         </div>
-        <div>
+        <div className='block lg:hidden'>
           <p>VIEW CODE</p>
-          <hr/>
+          <hr className='w-[100px] md:w-[90px] my-2 border-[green] border-t-2 font-bold ' />
         </div>
         </div>
         </div>
-
+        
+</div>
         <div>
         <img src={bitmap2} className='w-345 h-253 left-15' />
-        <h2>TODO WEB APP</h2>
+        <h2 className=' text-2xl md:text-4xl lg:text-5xl ' >TODO WEB APP</h2>
         <div>
           <p>HTML</p>
           <p>CSS</p>
@@ -110,17 +117,17 @@ const Content = () => {
         <div>
         <div>
           <p>VIEW PROJECT</p>
-          <hr/>
+          <hr className='w-[100px] md:w-[340px] my-2 border-[green] border-t-2 font-bold ' />
         </div>
         <div>
           <p>VIEW CODE</p>
-          <hr/>
+          <hr className='w-[100px] md:w-[340px] my-2 border-[green] border-t-2 font-bold ' />
         </div>
         </div>
         </div>
         <div>
         <img src={bitmap3} className='w-345 h-253 left-15' />
-        <h2>ENTERTAINMENT WEB APP</h2>
+        <h2 className=' text-2xl md:text-4xl lg:text-5xl '>ENTERTAINMENT WEB APP</h2>
         <div>
           <p>HTML</p>
           <p>CSS</p>
@@ -138,7 +145,7 @@ const Content = () => {
         </div>
         </div >
         <img src={bitmap4} className='w-345 h-253 left-15' />
-        <h2>MEMORY GAME</h2>
+        <h2 className=' text-2xl md:text-4xl lg:text-5xl '>MEMORY GAME</h2>
         <div>
           <p>HTML</p>
           <p>CSS</p>
@@ -147,15 +154,15 @@ const Content = () => {
         <div>
         <div>
           <p>VIEW PROJECT</p>
-          <hr/>
+          <hr className='w-[100px] md:w-[340px] my-2 border-[green] border-t-2 font-bold ' />
         </div>
         <div>
           <p>VIEW CODE</p>
-          <hr/>
+          <hr className='w-[100px] md:w-[340px] my-2 border-[green] border-t-2 font-bold ' />
         </div>
         </div>
         <img src={bitmap5} className='w-345 h-253 left-15' />
-        <h2>ART GALLERY SHOWCASE</h2>
+        <h2 className=' text-2xl md:text-4xl lg:text-5xl '>ART GALLERY SHOWCASE</h2>
         <div>
           <p>HTML</p>
           <p>CSS</p>
@@ -164,15 +171,15 @@ const Content = () => {
         <div>
         <div>
           <p>VIEW PROJECT</p>
-          <hr/>
+          <hr className='w-[100px] md:w-[340px] my-2 border-[green] border-t-2 font-bold ' />
         </div>
         <div>
           <p>VIEW CODE</p>
-          <hr/>
+          <hr className='w-[100px] md:w-[340px] my-2 border-[green] border-t-2 font-bold ' />
         </div>
         </div>
         <div>
-          <h2>Contact</h2>
+          <h2 className=' text-2xl md:text-4xl lg:text-5xl '>Contact</h2>
           <p>I would love to hear about your project and how I could help. Please fill in the form, and I’ll get back to you as soon as possible.</p>
           <div>
             <div>
