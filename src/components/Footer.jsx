d4af457bf927b0af5a0775b8ca45206c26e8b878
tiwavaldese <tiwaval7@gmail.com>
2024-09-05 14:17:35 +0100

    adding a handle click on the icons so when clicked it can change colour from white to green

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
+import React,  { useState } from 'react'
 import { FiGithub } from 'react-icons/fi';
 import { FaLinkedin } from 'react-icons/fa';
 import { FaTwitter } from 'react-icons/fa';
 import complexshape from '../assets/complexshape.png'
 
 const Footer = () => {
+  const [clickedIcon, setClickedIcon] = useState(null);
+  const handleIconClick = (icon) => {
+    setClickedIcon(icon); 
+  };
+
   return (
     <div className=' px-3 py-10 md:space-x-8 lg:space-x-8 bg-dark-grey '>
       
@@ -38,13 +43,23 @@ const Footer = () => {
         <div className='flex justify-center'>
         <hr className='w-[300px] md:w-[500px] lg:w-[1110px]  my-2  border-t-2 font-bold '/>
         </div>
-      <div className='flex flex- px-0 md:px-32 lg:px-14 mt-7 md:flex-row lg:flex-row justify-center items-center  md:justify-between lg:justify-between '>
+      <div className='flex flex-col px-0 md:px-32 lg:px-20 mt-7 md:flex-row lg:flex-row justify-center items-center  md:justify-between lg:justify-between '>
         <h2>adamkeyes</h2>
         <div className='flex items-center py-4 md:py-0 lg:py-0 gap-5 justify-center'>
-          <FiGithub />
-          <img src={complexshape} className='text-black' alt='' />
-          <FaLinkedin />
-          <FaTwitter />
+          <FiGithub  className={`cursor-pointer ${clickedIcon === 'github' ? 'text-green-500' : 'text-white'}`}
+        onClick={() => handleIconClick('github')} 
+        size={20} />
+          <img src={complexshape} 
+        className={`cursor-pointer ${clickedIcon === 'complexshape' ? 'text-green-500' : 'text-white'}`} 
+        alt='' 
+        onClick={() => handleIconClick('complexshape')} />
+          <FaLinkedin className={`cursor-pointer ${clickedIcon === 'linkedin' ? 'text-green-500' : 'text-white'}`} 
+        onClick={() => handleIconClick('linkedin')} 
+        size={20} 
+ />
+          <FaTwitter  className={`cursor-pointer ${clickedIcon === 'twitter' ? 'text-green-500' : 'text-white'}`} 
+        onClick={() => handleIconClick('twitter')} 
+        size={20} />
         </div>
       </div>
     </div>
